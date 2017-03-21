b61517d785726ff20e8f6c7b8bd445fb1cf8c95f
Kentaro KAZAMA <kazehara@outlook.com>
2017-03-21 21:54:05 +0900

    Add a command line argument

--- src/run.cpp
+++ src/run.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
   int nTx;
   int nRx;
   int nC;
+  int nsnr;
   char modulation[16];
   char inputName[256];
   char outputName[256];
@@ -13,19 +14,22 @@ int main(int argc, char *argv[]) {
   itpp::bvec transmitBits, receivedBits;
 
 
-  if (argc != 7) {
-    std::cout << "Usage: ./sm nTx nRx nC Tc inputImageFilename OutputCSVFilename"
+  if (argc != 8) {
+    std::cout << "Usage: ./simulator [Number of antennas to transmit] [Number of antennas to receive] [Constellation size] [dB] [sm|smx] [Image file to transmit] [Output csv]"
               << std::endl
-              << "Example: ./sm 2 2 4 sm lena.bmp output.csv  (2x2 QPSK on fast fading)"
+              << "Example: ./simulator 2 2 4 10 sm lena.bmp output.csv  (2x2 QPSK Spatial Modulation at 10dB)"
+              << std::endl
+              << "Example: ./simulator 4 2 8 0 smx lena.bmp output.csv  (4x2 8PSK Spatial Multiplexing at 0dB)"
               << std::endl;
     exit(1);
   } else {
     sscanf(argv[1], "%i", &nTx);
     sscanf(argv[2], "%i", &nRx);
     sscanf(argv[3], "%i", &nC);
-    sscanf(argv[4], "%s", modulation);
-    sscanf(argv[5], "%s", inputName);
-    sscanf(argv[6], "%s", outputName);
+    sscanf(argv[4], "%i", &nsnr);
+    sscanf(argv[5], "%s", modulation);
+    sscanf(argv[6], "%s", inputName);
+    sscanf(argv[7], "%s", outputName);
   }
 
 
@@ -33,18 +37,17 @@ int main(int argc, char *argv[]) {
   strBits = encoder.get_bytes_of_image();
 
   transmitBits = encoder.convert(strBits);
-  //transmitBits = itpp::randb(1e9);
 
   if (strcmp(modulation, "sm") == 0) {
     SMSimulator sm(nTx, nRx, nC, 1, outputName);
     sm.init(transmitBits);
 
-    receivedBits = sm.simulate(30);
+    receivedBits = sm.simulate(nsnr);
   } else {
     SMxSimulator smx(nTx, nRx, nC, 1, outputName);
     smx.init(transmitBits);
 
-    receivedBits = smx.simulate(30);
+    receivedBits = smx.simulate(nsnr);
   }
 
   encoder.decode(receivedBits);
